532ff0f38ec2a9c58565404ba30a94afe2b2855d
Mirko Saenz Valiente <saenzvalientemirko@gmail.com>
2023-10-30 23:43:43 -0300

    project created - protected route added

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { join } from "node:path"
+import { mainRoutes } from "./routes/main-router.js"
+
+const app = express()
+
+app.use(express.static(join(process.cwd(), "web")))
+app.use(express.json())
+
+app.use(mainRoutes())
+
+app.listen(3000, () => { console.log("SERVER LISTENING ON PORT 3000") })
+
